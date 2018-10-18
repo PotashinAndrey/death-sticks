fbd97a086f384eb68eecb0131073662e2836a185
Андрей Поташин <PotashinAndrey@users.noreply.github.com>
2018-10-19 01:33:49 +0500

    доработка сайта, работа с дата.джсон

--- PWAMain/script.js
+++ PWAMain/script.js
@@ -11,48 +11,48 @@
 
 window.addEventListener('DOMContentLoaded', ready);
 
-var data, preview, output;
+var data, preview, output, array = [];
 
 async function ready() {
-  data = await fetch('./data.json').then(response => response.json());
-  var input = document.querySelector('input[type="file"]');
-  preview = document.querySelector('img');
-  output = document.querySelector('pre');
+    data = await fetch('./data.json').then(response => response.json());
+    var input = document.querySelector('input[type="file"]');
+    preview = document.querySelector('img');
+    output = document.querySelector('pre');
 
-  input.addEventListener('change', getImage);
-
+    input.addEventListener('change', getImage);
 }
 
 function getImage(event) {
-  var file = event.target.files[0];
-  var reader = new FileReader();
-  var image;
+    var file = event.target.files[0];
+    var reader = new FileReader();
+    var image;
 
-  reader.onloadend = function () {
-    image = reader.result;
-    preview.src = image;
+    reader.onloadend = function () {
+        image = reader.result;
+        // preview.src = image;
 
-    Quagga.decodeSingle({
-          decoder: {
-              readers: ["ean_reader", "code_128_reader"] // List of active readers
-          },
-          locate: true, // try to locate the barcode in the image
-          src: image // '/test/fixtures/code_128/image-001.jpg' // or 'data:image/jpg;base64,' + data
-      }, function(result){
-          console.log(result);
-          if (!(result && result.codeResult && result.codeResult.code)) {
-            // output.innerText = "Не распозналось";
-            return;
-          }
-          // output.innerText = result.codeResult.code; // JSON.stringify(result, null, 2);
-          // output.innerText = result.codeResult.code + '\n\n' + JSON.stringify(data[result.codeResult.code], null, 2);
-          if(result.codeResult) {
-              console.log("result", result.codeResult.code);
-          } else {
-              console.log("not detected");
-          }
-      });
-  }
+        Quagga.decodeSingle({
+            decoder: {
+                readers: ["ean_reader", "code_128_reader"] // List of active readers
+            },
+            locate: true, // try to locate the barcode in the image
+            src: image // '/test/fixtures/code_128/image-001.jpg' // or 'data:image/jpg;base64,' + data
+        }, function (result) {
+            console.log(result);
+            if (!(result && result.codeResult && result.codeResult.code)) {
+                alert("Не распозналось");
+                return;
+            }
+            var tabletName = data[result.codeResult.code]["name"];
+            if (!array.includes(tabletName)) array.push(tabletName);
+            output.innerText = array;
+            if (result.codeResult) {
+                console.log("result", result.codeResult.code);
+            } else {
+                console.log("not detected");
+            }
+        });
+    }
 
-  reader.readAsDataURL(file);
+    reader.readAsDataURL(file);
 }
